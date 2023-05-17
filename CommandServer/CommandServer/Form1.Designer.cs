122302fdb78f270bb4795ad29454d249eab04804
unknown <thanhnguyenphuoc97@gmail.com>
2023-05-17 07:26:32 +0700

    add ssh-connection

--- CommandServer/CommandServer/Form1.Designer.cs
+++ CommandServer/CommandServer/Form1.Designer.cs
@@ -40,9 +40,21 @@
             textBox1 = new TextBox();
             tabPage2 = new TabPage();
             tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            textBoxThucThi = new TextBox();
+            btConnection = new Button();
+            textBoxPassword = new TextBox();
+            label4 = new Label();
+            textBoxUsername = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            texBoxHostname = new TextBox();
+            label1 = new Label();
+            textBoxHienThi = new TextBox();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             statusStrip1.SuspendLayout();
+            tabPage4.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
@@ -50,6 +62,7 @@
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
             tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
             tabControl1.Location = new Point(0, 0);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
@@ -143,7 +156,7 @@
             textBox1.Location = new Point(256, 6);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(536, 294);
+            textBox1.Size = new Size(536, 318);
             textBox1.TabIndex = 0;
             // 
             // tabPage2
@@ -166,6 +179,113 @@
             tabPage3.Text = "Key Logger";
             tabPage3.UseVisualStyleBackColor = true;
             // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(textBoxThucThi);
+            tabPage4.Controls.Add(btConnection);
+            tabPage4.Controls.Add(textBoxPassword);
+            tabPage4.Controls.Add(label4);
+            tabPage4.Controls.Add(textBoxUsername);
+            tabPage4.Controls.Add(label3);
+            tabPage4.Controls.Add(label2);
+            tabPage4.Controls.Add(texBoxHostname);
+            tabPage4.Controls.Add(label1);
+            tabPage4.Controls.Add(textBoxHienThi);
+            tabPage4.Location = new Point(4, 24);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(795, 421);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "SSH conection";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // textBoxThucThi
+            // 
+            textBoxThucThi.Location = new Point(290, 317);
+            textBoxThucThi.Name = "textBoxThucThi";
+            textBoxThucThi.Size = new Size(494, 23);
+            textBoxThucThi.TabIndex = 9;
+            textBoxThucThi.KeyDown += textBoxThucThi_KeyDown;
+            // 
+            // btConnection
+            // 
+            btConnection.Location = new Point(47, 349);
+            btConnection.Name = "btConnection";
+            btConnection.Size = new Size(180, 52);
+            btConnection.TabIndex = 8;
+            btConnection.Text = "Connection";
+            btConnection.UseVisualStyleBackColor = true;
+            btConnection.Click += btConnection_Click;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(84, 164);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.PasswordChar = '*';
+            textBoxPassword.Size = new Size(143, 23);
+            textBoxPassword.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 167);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Password :";
+            // 
+            // textBoxUsername
+            // 
+            textBoxUsername.Location = new Point(84, 121);
+            textBoxUsername.Name = "textBoxUsername";
+            textBoxUsername.Size = new Size(143, 23);
+            textBoxUsername.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(5, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Hostname :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(5, 129);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Username :";
+            // 
+            // texBoxHostname
+            // 
+            texBoxHostname.Location = new Point(84, 89);
+            texBoxHostname.Name = "texBoxHostname";
+            texBoxHostname.Size = new Size(143, 23);
+            texBoxHostname.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(49, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(207, 37);
+            label1.TabIndex = 1;
+            label1.Text = "SSH Connection";
+            // 
+            // textBoxHienThi
+            // 
+            textBoxHienThi.BackColor = Color.Black;
+            textBoxHienThi.ForeColor = Color.Green;
+            textBoxHienThi.Location = new Point(290, 6);
+            textBoxHienThi.Multiline = true;
+            textBoxHienThi.Name = "textBoxHienThi";
+            textBoxHienThi.Size = new Size(494, 287);
+            textBoxHienThi.TabIndex = 0;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -181,6 +301,8 @@
             tabPage1.PerformLayout();
             statusStrip1.ResumeLayout(false);
             statusStrip1.PerformLayout();
+            tabPage4.ResumeLayout(false);
+            tabPage4.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -198,5 +320,16 @@
         private TextBox textBox1;
         private TabPage tabPage2;
         private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TextBox textBoxThucThi;
+        private Button btConnection;
+        private TextBox textBoxPassword;
+        private Label label4;
+        private TextBox textBoxUsername;
+        private Label label3;
+        private Label label2;
+        private TextBox texBoxHostname;
+        private Label label1;
+        private TextBox textBoxHienThi;
     }
 }
--- CommandServer/CommandServer/Form1.cs
+++ CommandServer/CommandServer/Form1.cs
@@ -1,10 +1,11 @@
-using System;
+﻿using System;
 using System.Text;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using System.IO; //for Streams
 using System.Threading; //to run commands concurrently
 using System.Net; //for IPEndPoint
+using Renci.SshNet;
 
 namespace CommandServer
 {
@@ -39,6 +40,7 @@ namespace CommandServer
             th_StartListen = new Thread(new ThreadStart(StartListen));
             th_StartListen.Start();
             textBox2.Focus();
+            textBoxThucThi.Focus();
         }
 
         private void StartListen()
@@ -164,6 +166,64 @@ namespace CommandServer
         {
             Cleanup();
             System.Environment.Exit(0);
+        }
+
+        private void btConnection_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void RunCommand()
+        {
+            var host = texBoxHostname.Text;
+            var username = textBoxUsername.Text;
+            var password = textBoxPassword.Text;
+
+            using (var client = new SshClient(host, username, password))
+            {
+                client.Connect();
+                // If the command2 depend on an environment modified by command1,
+                // execute them like this.
+                // If not, use separate CreateCommand calls.
+                var cmd = client.CreateCommand("command1; command2");
+
+                var result = cmd.BeginExecute();
+
+                using (var reader = new StreamReader(
+                                      cmd.OutputStream, Encoding.UTF8, true, 1024, true))
+                {
+                    while (!result.IsCompleted || !reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (line != null)
+                        {
+                            textBoxHienThi.Invoke(
+                                (MethodInvoker)(() =>
+                                    textBoxHienThi.AppendText(line + Environment.NewLine)));
+                        }
+                    }
+                }
+
+                cmd.EndExecute(result);
+            }
+        }
+
+        private void textBoxThucThi_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    strInput.Append(textBox2.Text.ToString());
+                    streamWriter.WriteLine(strInput);
+                    streamWriter.Flush();
+                    strInput.Remove(0, strInput.Length);
+                    if (textBox2.Text == "exit") Cleanup();
+                    if (textBox2.Text == "terminate") Cleanup();
+                    if (textBox2.Text == "cls") textBox1.Text = "";
+                    textBox2.Text = "";
+                }
+            }
+            catch (Exception err) { }
         }
     }
 }
